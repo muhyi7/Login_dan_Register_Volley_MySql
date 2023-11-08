7bceada06519e61b2d4fb0c5d4f09766471282e4
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-09 06:21:55 +0700

    Finish dengan Ip dinamis di db contract

---
+++ app/src/main/java/com/google/logindanregistervolleymysql/Db_Contract.java
@@ -0,0 +1,10 @@
+package com.google.logindanregistervolleymysql;
+
+public class Db_Contract {
+
+    public static String ip = "192.168.1.15";
+
+    public static final String urlRegister = "http://"+ip+"//my_api/api-register.php";
+    public static final String urlLogin = "http://"+ip+"//my_api/api-login.php";
+
+}
